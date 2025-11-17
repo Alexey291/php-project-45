b470ffdc026f64582c81e487c957cc47d57edde0
Алексей Чеузов <alexeyblack77@gmail.com>
2025-11-17 20:17:57 +0300

    5

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 brain-games:
 	composer validate
 	php bin/brain-games
+
+brain-even:
+	composer validate
+	php bin/brain-even
--- src/Cli.php
+++ src/Cli.php
@@ -8,9 +8,11 @@ use function cli\prompt;
 
 class Cli
 {
-    public static function greetUser(): void
+    public static function greetUser(): string
     {
         $name = prompt('May I have your name?');
         line("Hello, %s!", $name);
+
+        return $name;
     }
 }
